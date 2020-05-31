2ce5be646920f9baf0e23428aff0dee81b39b94c
Eliseu Videira <eliseuvideira1@gmail.com>
2020-05-31 14:45:22 -0300

    Executing static queries to display a table with data from files

--- src/pages/posts/page-3.js
+++ src/pages/posts/page-3.js
@@ -1,11 +1,52 @@
 import React from 'react'
 import Layout from '../../components/layout'
-import { Link } from 'gatsby'
+import { Link, useStaticQuery } from 'gatsby'
 
 export default function Page3() {
+  const data = useStaticQuery(graphql`
+    {
+      allFile {
+        totalCount
+        edges {
+          node {
+            relativePath
+            size
+            extension
+            birthTime
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <Layout>
       <h1>Hello from Page 3!</h1>
+
+      <h3>Image File Data</h3>
+      <table>
+        <thead>
+          <tr>
+            <td>Relative Path</td>
+            <td>Size</td>
+            <td>Extension</td>
+            <td>Birthtime</td>
+          </tr>
+        </thead>
+        <tbody>
+          {data.allFile.edges.map(
+            ({ node: { relativePath, size, extension, birthTime } }, index) => (
+              <tr>
+                <td>{relativePath}</td>
+                <td>{size}</td>
+                <td>{extension}</td>
+                <td>{birthTime}</td>
+              </tr>
+            ),
+          )}
+        </tbody>
+      </table>
+
       <Link to="/page-2">Go to page 2</Link>
     </Layout>
   )
