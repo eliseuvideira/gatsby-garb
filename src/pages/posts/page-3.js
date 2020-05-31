4b0f3d303e453e0b9342d92b8137b7f8e0213c15
Eliseu Videira <eliseuvideira1@gmail.com>
2020-05-31 13:58:41 -0300

    Using the Layout and Link components

--- src/pages/posts/page-3.js
+++ src/pages/posts/page-3.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Layout from '../../components/layout'
+import { Link } from 'gatsby'
 
 export default function Page3() {
   return (
-    <div>
+    <Layout>
       <h1>Hello from Page 3!</h1>
-    </div>
+      <Link to="/page-2">Go to page 2</Link>
+    </Layout>
   )
 }
