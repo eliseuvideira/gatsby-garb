4659aa5ecac280b57ee1ab34bdf436acd5da4026
Eliseu Videira <eliseuvideira1@gmail.com>
2020-05-31 23:54:19 -0300

    Formatting date using graphql and displaying time to read inside the post

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -81,7 +81,7 @@ export const query = graphql`
           id
           frontmatter {
             title
-            date
+            date(formatString: "MMMM Do, YYYY")
           }
           excerpt
         }
