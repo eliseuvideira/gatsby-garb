a694ba10ac933af7f33161429f7fe9945a207b47
Eliseu Videira <eliseuvideira1@gmail.com>
2020-05-31 23:59:22 -0300

    Working with image in gatsby

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,8 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import gatsbyLogo from '../images/gatsby-icon.png'
 
 const Header = ({ siteTitle }) => (
   <header
@@ -16,17 +18,29 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
+      <span style={{ display: 'flex', alignItems: 'center' }}>
+        <img
+          src={gatsbyLogo}
+          alt="Gatsby Garb Logo"
           style={{
-            color: `white`,
-            textDecoration: `none`,
+            width: 50,
+            margin: '0 5px',
+            border: '3px solid orange',
+            borderRadius: '50%',
           }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+        />
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+      </span>
     </div>
   </header>
 )
