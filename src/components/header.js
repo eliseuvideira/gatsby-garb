301b86c41964355e97fb24620a2330efa8754b2a
Eliseu Videira <eliseuvideira1@gmail.com>
2020-06-02 23:03:47 -0300

    Added cart information at the header

--- src/components/header.js
+++ src/components/header.js
@@ -13,6 +13,9 @@ const Header = ({ siteTitle }) => (
   >
     <div
       style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
         margin: `0 auto`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
@@ -41,6 +44,28 @@ const Header = ({ siteTitle }) => (
           </Link>
         </h1>
       </span>
+      <div
+        style={{ color: 'white', cursor: 'pointer' }}
+        className="snipcart-summary snipcart-checkout"
+      >
+        <div>
+          <strong>My Cart</strong>
+        </div>
+        <div>
+          <span
+            style={{ fontWeight: 'bold' }}
+            className="snipcart-total-items"
+          ></span>{' '}
+          Items in cart
+        </div>
+        <div>
+          Total price{' '}
+          <span
+            style={{ fontWeight: 'bold' }}
+            className="snipcart-total-price"
+          ></span>
+        </div>
+      </div>
     </div>
   </header>
 )
